863d3793b2159e1258dde6ca4e66547dab0baa78
EddieBenn <ndaobongedidiong56@gmail.com>
2023-09-06 22:48:24 +0100

    working version

--- src/config/index.ts
+++ src/config/index.ts
@@ -2,14 +2,19 @@ import {Sequelize} from 'sequelize';
 import dotenv from 'dotenv';
 dotenv.config();
 
-export const db = new Sequelize(process.env.CONNECTION_STRING!, 
+const {DB_PORT,
+    DB_NAME,
+    DB_USER,
+    DB_PASSWORD} = process.env
+export const db = new Sequelize(
+    DB_NAME!,
+    DB_USER!,
+    DB_PASSWORD,
     {
+        host: "localhost",
+        port: DB_PORT as unknown as number,
         logging: false,
-        dialectOptions: {
-            ssl: {
-                require: false
-            }
-        }
+        dialect: "postgres"
     })
 
 // export const db = new Sequelize(process.env.DB_NAME as string, process.env.DB_USER as string, process.env.DB_PASSWORD as string, {
@@ -21,9 +26,9 @@ export const db = new Sequelize(process.env.CONNECTION_STRING!,
 
 
 //SENDING OTP TO PHONE
-export const accountSid = process.env.ACCOUNTSID;
-export const authToken = process.env.AUTHTOKEN
-export const fromAdminPhone = process.env.FROMADMINPHONE
+export const accountSid = process.env.AccountSid;
+export const authToken = process.env.AuthToken
+export const fromAdminPhone = process.env.fromAdninPhone
 
 //SENDING OTP TO EMAIL
 export const GMAIL_USER = process.env.Gmail
