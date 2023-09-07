9a182efc2c67ebcf5e7b1873fec8f6145410bd3a
EddieBenn <ndaobongedidiong56@gmail.com>
2023-09-07 16:42:25 +0100

    Corrections made to get request

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,10 +1,19 @@
 import express, {Request, Response} from 'express';
+import { UserInstance } from '../models/userModel';
+import { OrderInstance } from '../models/orderModel';
 
 const router = express.Router();
 
-router.get('/', (req:Request, res:Response) => {
- res.status(200).send(`WELCOME TO SWIFT RIDER, CLICK TO VIEW DOCUMENTATION`)
+router.get('/', async (req:Request, res:Response) => {
+    const allUsers = await UserInstance.findAll({})
+    const allOrders = await OrderInstance.findAll({})
+    return res.status(200).json({
+        message: `All Orders Fetched`,
+         Orders: allOrders,
+         Users: allUsers,
+    })
 })
 
+// res.status(200).send(`WELCOME TO SWIFT RIDER, CLICK TO VIEW DOCUMENTATION`)
 
 export default router;
